ce564adcecfbc9825ceecf87c5606b050acf4eca
Sandip Mondal <113540636+sandip9593@users.noreply.github.com>
2024-11-18 11:07:37 +0530

    Add files via upload

---
+++ DSA-C++/boom_defuse.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+         int n=code.size();
+        vector<int> ans(n,0);
+         
+         if(k==0)
+             return ans;
+         
+        for(int i=0; i<n; i++)
+        {
+            int j=0;
+            int sum=0;
+            int count=k;
+            if(k>0)
+            {
+                 j=i+1;
+                while(count--)
+                {
+                    sum+=code[j%n];
+                    j++;
+                }
+            }
+            else
+            {
+                if(i==0)
+                   j=n-1;
+                else
+                   j=i-1;
+                
+                while(count++)
+                {
+                    sum+=code[j];
+                    if(j==0)
+                        j=n-1;
+                    else
+                        j--;
+                }
+            }
+            ans[i]=sum;
+        }
+        return ans;
+    }
+};
